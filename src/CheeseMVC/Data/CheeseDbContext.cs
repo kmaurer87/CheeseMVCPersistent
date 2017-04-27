63fdb29de38a13e23cad723cb14719dec45b8f28
User <User@WINDEV1612EVAL>
2017-04-26 19:18:35 -0700

    Finished

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -1,12 +1,25 @@
 ﻿using CheeseMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Controllers;
+using CheeseMVC.Migrations;
+
+
 
 namespace CheeseMVC.Data
 {
     public class CheeseDbContext : DbContext
     {
         public DbSet<Cheese> Cheeses { get; set; }
+        public DbSet<CheeseCategory> Categories { get; set; }
+        public DbSet<CheeseMenu> CheeseMenus { get; set; }
+        public DbSet<Menu> Menus { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CheeseMenu>()
+                .HasKey(c => new { c.CheeseID, c.MenuID });
+        }
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
             : base(options)
         { }
---
+++ src/CheeseMVC/ViewModels/ViewMenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using CheeseMVC.Models;
+
+
+namespace CheeseMVC.ViewModels
+{
+    public class ViewMenuViewModel
+    {
+        public IList<CheeseMenu> Items { get; set; }
+        public Menu Menu { get; set; }
+    }
+}
